795f0d360db93edbdc21861da0fb707a057b73eb
saigovardhan <saigovardhan1123@gmail.com>
2026-08-26 23:42:43 +0530

    Fix deployment

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://www.thiranex.in",
+            "https://thiranex.in"
+        ],
+        credentials: true
+    })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
